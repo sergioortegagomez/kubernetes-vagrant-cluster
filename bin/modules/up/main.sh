4bded7fb292f2445390393fc84e0cee7730ebf66
Sergio Ortega <sergio.ortega.gomez@gmail.com>
2019-09-21 12:38:00 +0200

    Update: Add call restart module

--- bin/modules/up/main.sh
+++ bin/modules/up/main.sh
@@ -2,10 +2,11 @@
 
 function main() {
     vagrant up
+    bash platform-control.sh restart
 }
 
 function help() {
-    echo " - up: platform up!! :) "
+    echo " - up: platform up :) "
 }
 
 [ $# == 0 ] && help || main
